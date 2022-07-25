776f42f96e1b0c053b3334fb988dccab1cfbce43
Araati <araati.the.fox@gmail.com>
2022-07-25 23:20:47 +0300

    scheduler module created

---
+++ scheduler/src/main/java/com/newestworld/scheduler/SchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.newestworld.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerApplication.class, args);
+    }
+
+}
