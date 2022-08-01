e4ead3700b85255f8bde279c553e72e8241d3b62
Araati <araati.the.fox@gmail.com>
2022-08-02 00:42:31 +0300

    notprod profile for TestDataLoader in scheduler

--- scheduler/src/main/java/com/newestworld/scheduler/TestDataLoader.java
+++ scheduler/src/main/java/com/newestworld/scheduler/TestDataLoader.java
@@ -4,9 +4,11 @@ import com.newestworld.scheduler.dao.ActionTimeoutRepository;
 import com.newestworld.scheduler.model.entity.ActionTimeoutEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@Profile("!prod")
 @Component
 @RequiredArgsConstructor
 public class TestDataLoader {
