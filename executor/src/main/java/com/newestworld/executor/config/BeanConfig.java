85cb34b11d28901ee566823d36408fe427f7c859
Araati <araati.the.fox@gmail.com>
2022-08-03 18:17:49 +0300

    added BeanConfig in executor

--- executor/src/main/java/com/newestworld/executor/config/BeanConfig.java
+++ executor/src/main/java/com/newestworld/executor/config/BeanConfig.java
@@ -1,2 +1,19 @@
-package com.newestworld.executor.config;public class BeanConfig {
+package com.newestworld.executor.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+        com.newestworld.streams.publisher.FactoryUpdatePublisher.class,
+        com.newestworld.streams.publisher.ActionDeletePublisher.class
+})
+@ComponentScan
+@EnableAutoConfiguration
+@RequiredArgsConstructor
+public class BeanConfig {
+
 }
