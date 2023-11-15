dbed7fe7ce4e9faa190a82888c3a20cc2f7b1556
Araati <araati.the.fox@gmail.com>
2023-11-15 23:29:09 +0300

    added profiles

--- dev.sh
+++ dev.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-docker rm newest-world_content_1 || true
-docker rm newest-world_executor_1 || true
-ocker rm newest-world_scheduler_1 || true
-docker rmi content || true
-docker rmi executor || true
-docker rmi scheduler || true
-mvn clean compile package -DskipTests=true
+export JAVA_HOME=/home/araati/.jdks/openjdk-16.0.2
+
+case $1 in
+--help|-h)
+  help
+  ;;
+
+infra)
+  docker compose --profile infra up -d
+  ;;
+
+rebuild)
+  docker rm newest-world_content_1 || true
+  docker rm newest-world_executor_1 || true
+  docker rm newest-world_scheduler_1 || true
+  docker rmi content || true
+  docker rmi executor || true
+  docker rmi scheduler || true
+  mvn clean compile package -DskipTests=true
+  ;;
+
+*);;
+esac
 
 exit 0
 
