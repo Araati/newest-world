d1ae4ead9934afb05f6cc8fe7d6c288c06a5d64f
Araati <araati.the.fox@gmail.com>
2024-05-05 14:53:08 +0300

    validator service created and configured

--- dev.sh
+++ dev.sh
@@ -13,9 +13,11 @@ rebuild)
   docker rm newest-world_content_1 || true
   docker rm newest-world_executor_1 || true
   docker rm newest-world_scheduler_1 || true
+  docker rm newest-world_validator_1 || true
   docker rmi content || true
   docker rmi executor || true
   docker rmi scheduler || true
+  docker rmi validator || true
   mvn clean compile package -DskipTests=true
   ;;
 
